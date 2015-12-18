56fa0ae4eef0d30af4f18c4bc2ae36d0af962298
Kevin Oh <aflashyrhetoric@gmail.com>
2015-12-18 09:20:52 -0500

    Initial commit. Configured Foundation to work with Laravel 5.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="UTF-8">
+       <title></title>
+       <link rel="stylesheet" href="css/app.css">
+
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        
+        @yield('footer')
+    </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+@section('content')
+
+<div class="success callout">
+  <p>Created a brand new folder.</p>
+</div>
