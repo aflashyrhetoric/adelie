49f32e864a5703e668d45b9b6d3b719cac93ecb6
aflashyrhetoric@gmail.com <aflashyrhetoric@gmail.com>
2016-01-07 21:03:14 -0500

    Finished meet page.

--- resources/views/partials/meet.blade.php
+++ resources/views/partials/meet.blade.php
@@ -1,13 +1,36 @@
 @extends('app')
 @section('content')
 <div class="row meet-container">
-  <div class="meet-inner-container small-12 small-centered medium-6 columns">
-    <h1>Meet The Adelie Team</h1>
-    <p>Introducing the duo behind the name.</p>
+  <div class="meet-header-container small-12 small-centered medium-12 columns">
+    <h1>meet the adelie team</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
   </div>
 
-  <div class="kevin">
-    
+  <!-- Kevin -->
+  <div class="kevin team-member small-12 medium-6 large-5 columns boxed">
+    <img src="http://lorempixel.com/175/175/" alt="kevin">
+    <h2>kevin</h2>
+    <h4>web developer</h4>
+    <hr>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel ab aspernatur nihil consequuntur tempore. Perspiciatis et ullam sit, possimus vel nobis, soluta, consectetur aspernatur, recusandae quidem minima dolor rem asperiores.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere, qui! Tempora dicta delectus voluptates itaque magni facilis quis aut maxime, quibusdam assumenda dignissimos vero consectetur atque minima, facere eum laboriosam labore odio! Nisi assumenda sapiente doloremque nulla ipsa porro ex nemo atque eum, fuga?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore porro neque, vero, ab possimus architecto aliquid maiores amet ducimus voluptas soluta hic, assumenda dolor perspiciatis.</p>
+    <hr>
+    <p>You can email me at <a href="mailto:koh@adelieweb.com">koh@adelieweb.com.</a></p>
+    <a class="button" href="/services/webdev">Read about our web development services.</a>
+  </div>
+  <div class="yuna team-member small-12 medium-6 large-5 large-offset-1 columns boxed">
+    <img src="http://lorempixel.com/175/175/" alt="yuna">
+    <h2>vanessa</h2>
+    <h4>design & brand management</h4>
+    <hr>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, fugiat, recusandae. Earum illo recusandae adipisci cum consequuntur. Aperiam cupiditate sint incidunt iure, ipsum hic accusamus provident! Eveniet atque, cumque maxime?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ipsa perspiciatis laudantium, atque dolores pariatur voluptatem mollitia illum harum nam? Alias provident et, eaque. Voluptatem natus, exercitationem reiciendis, laboriosam eum incidunt aliquid dolores laborum, possimus vel repudiandae quae repellendus! Omnis maxime molestiae enim, dolores voluptates.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quia sunt praesentium, facilis aliquid a quaerat, obcaecati cum repudiandae qui expedita. Eveniet quasi nesciunt perspiciatis!</p>    
+    <hr>
+    <a class="button" href="/services/graphic_design">To learn more about our graphic design services, click here!</a>
+    <a class="button" href="/services/branding">To learn more about our web branding services, click here!</a>
   </div>
 </div>
 @endsection
