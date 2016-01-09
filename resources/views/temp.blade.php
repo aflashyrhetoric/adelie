fb15d848f8b1e10340024243ed9d2e2d7a1913dd
aflashyrhetoric@gmail.com <aflashyrhetoric@gmail.com>
2016-01-08 22:31:10 -0500

    Tweaked background colors for temp page.

--- resources/views/temp.blade.php
+++ resources/views/temp.blade.php
@@ -6,43 +6,64 @@
     <script src="https://use.typekit.net/ghk7jiq.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
     <style>
-    .temp-container {
+    body {
+    background-color: #3A539B;
     display: -webkit-flex;
     display: -moz-flex;
     display: -ms-flex;
     display: -o-flex;
     display: flex;
+    height: 100%;
+    flex-flow: row nowrap;
     align-items: center;
     justify-content: center;
-    text-align: center;
     }
     .temp{
-      display: -webkit-flex;
-      display: -moz-flex;
-      display: -ms-flex;
-      display: -o-flex;
-      display: flex;
-      flex-flow: column nowrap;
-      align-items: center;
-      justify-content: center;
-    }
-    .temp h1, .temp p, .temp h4 {
-    font-family: 'Raleway';
     display: -webkit-flex;
     display: -moz-flex;
     display: -ms-flex;
     display: -o-flex;
     display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    justify-content: center;
+    }
+    .temp h1, .temp p, .temp h4, .temp a {
+    font-family: 'Raleway';
+    color: white;
+    text-decoration: none;
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: flex;
+    }
+    .temp h1 {
+      font-family: 'corporate-e';
+      letter-spacing: -2px;
+    }
+    .temp a{
+    /*color: #3a539b;*/
+    color: white;
+    text-decoration: underline;
+    height: 1rem;
+    width: auto;
+    padding: 1rem;
+    padding-top: 0.8rem;
+    background-color: transparent;
+    border: 1px solid white;
+    border-radius: 3px;
+    }
+    .temp a:hover {
     }
     </style>
   </head>
   <body>
-    <div class="temp-container">
-      <div class="temp">
-        <h1>Adelie</h1> <br>
-        <p>Our website is currently under construction.</p>
-        <h4>Questions? Contact us <a href="mailto:contact@adelieweb.com">here.</a></h4>
-      </div>
+    <div class="temp">
+      <h1>adelie</h1> <br>
+      <p>Our website is currently under construction.</p>
+      <h4>Questions?</h4>
+      <a href="mailto:contact@adelieweb.com">Contact us here!</a>
     </div>
   </body>
 </html>
