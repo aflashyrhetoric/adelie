17615a94087976566dce4deae38a38109c17e87f
Kevin Oh <aflashyrhetoric@gmail.com>
2016-03-16 17:23:34 -0400

    Further adjusted the way that foundation settings get imported. Added overwrites, modified filenames by suffixing 'modified' to the end of modified files, added a drilldown overwrite. Some other stuff.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,54 +1,50 @@
-<div data-sticky-container>
+<div data-sticky-container style="height: 65.3906px !important;">
   <div data-sticky data-sticky-on="medium" data-anchor="site-container" data-margin-top="0" style="width:100%;">
-
     <!-- Small Screen Menu Toggle -->
     <div class="title-bar" data-responsive-toggle="adelie-navbar" data-hide-for="medium">
       <button class="menu-icon" type="button" data-toggle></button>
       <div class="title-bar-title">Menu</div>
     </div>
-
     <!-- Topbar starts -->
-    <div class="top-bar" id="adelie-navbar">
+    <div class="top-bar navigation-bar" id="adelie-navbar">
       <div class="top-bar-left">
         <ul class="menu" data-dropdown-menu>
           <li id="adelie-logo" class="navbar-logo menu-text">adelie</li>
         </ul>
       </div>
       <div class="top-bar-right">
-        <ul class="dropdown menu hide-for-small-only show-for-medium-up" data-dropdown-menu>
-          <li><a href="/about">ABOUT</a></li>
-          <li><a href="https://medium.com/the-waddle" target="_blank">BLOG</a></li>
-          <li class="has-submenu">
+        <ul class="dropdown menu hide-for-small-only show-for-medium-up" data-dropdown-menu="" role="menubar" data-i="fw9l7j-i">
+          <li role="menuitem"><a href="/about" tabindex="0">ABOUT</a></li>
+          <li role="menuitem"><a href="https://medium.com/the-waddle" target="_blank">BLOG</a></li>
+          <li class="has-submenu is-dropdown-submenu-parent is-down-arrow" role="menuitem" aria-haspopup="true" aria-selected="false" aria-expanded="false" aria-label="SERVICES" data-is-click="false">
             <a href="#">SERVICES</a>
-            <ul class="submenu menu vertical" data-submenu>
-              <li><a href="/services/webdev">WEB DEVELOPMENT</a></li>
-              <li><a href="/services/graphic_design">GRAPHIC DESIGN</a></li>
-              <li><a href="/services/branding">BRANDING</a></li
-              >
+            <ul class="submenu menu vertical is-dropdown-submenu first-sub" data-submenu="" aria-hidden="true" role="menu">
+              <li role="menuitem" class="is-submenu-item is-dropdown-submenu-item"><a href="/services/webdev">WEB DEVELOPMENT</a></li>
+              <li role="menuitem" class="is-submenu-item is-dropdown-submenu-item"><a href="/services/graphic_design">GRAPHIC DESIGN</a></li>
+              <li role="menuitem" class="is-submenu-item is-dropdown-submenu-item"><a href="/services/branding">BRANDING</a></li>
             </ul>
           </li>
-          <li><a href="/meet">MEET THE TEAM</a></li>
-          <li><a href="/contact">CONTACT</a></li>
+          <li role="menuitem"><a href="/meet">MEET THE TEAM</a></li>
+          <li role="menuitem"><a href="/contact">CONTACT</a></li>
         </ul>
         <!--Small menu -->
-        <ul class="small-nav lstn show-for-small-only" data-drilldown>
-          <li><a href="/about">ABOUT</a></li>
-          <li><a href="https://medium.com/the-waddle">BLOG</a></li>
-          <li class="has-submenu">
-            <a href="#">SERVICES +</a>
-            <ul class="submenu vertical" data-submenu>
-              <li><a href="/services/webdev">WEB DEVELOPMENT</a></li>
-              <li><a href="/services/graphic_design">GRAPHIC DESIGN</a></li>
-              <li><a href="/services/branding">BRANDING</a></li
-              >
-            </ul>
-          </li>
-          <li><a href="/meet">MEET THE TEAM</a></li>
-          <li><a href="/contact">CONTACT</a></li>
-        </ul>
-      </div>
+        <div class="is-drilldown mobile-menu-overlay" style="height: 175px; width: 320px;"><ul class="small-nav lstn show-for-small-only" data-drilldown="" role="menubar" data-i="dmpzyg-i">
+          <li role="menuitem"><a href="/about" tabindex="0">ABOUT</a></li>
+          <li role="menuitem"><a href="https://medium.com/the-waddle">BLOG</a></li>
+          <li class="has-submenu is-drilldown-submenu-parent" role="menuitem" aria-haspopup="true" aria-selected="false" aria-expanded="false" aria-label="SERVICES">
+            <a>SERVICES +</a>
+            <ul class="submenu vertical is-drilldown-submenu" data-submenu="" aria-hidden="true" role="menu" tabindex="0"><li class="js-drilldown-back"><a>Back</a></li>
+            <li role="menuitem" class="is-submenu-item is-drilldown-submenu-item"><a href="/services/webdev">WEB DEVELOPMENT</a></li>
+            <li role="menuitem" class="is-submenu-item is-drilldown-submenu-item"><a href="/services/graphic_design">GRAPHIC DESIGN</a></li>
+            <li role="menuitem" class="is-submenu-item is-drilldown-submenu-item"><a href="/services/branding">BRANDING</a></li>
+          </ul>
+        </li>
+        <li role="menuitem"><a href="/meet">MEET THE TEAM</a></li>
+        <li role="menuitem"><a href="/contact">CONTACT</a></li>
+      </ul></div>
     </div>
-
-    <!-- Close out sticky -->
   </div>
+</div>
+<!-- Close out sticky -->
+</div>
 </div>
