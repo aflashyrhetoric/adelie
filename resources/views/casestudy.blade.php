5a31a55099c82b80f8b4b67c0cf58952c1bccdd2
Kevin Oh <aflashyrhetoric@gmail.com>
2016-04-07 14:55:34 -0400

    Case studies creation. Fixing the database configuration to not share 'homestead' across all sites. derp.

--- resources/views/casestudy.blade.php
+++ resources/views/casestudy.blade.php
@@ -3,8 +3,10 @@
 <div class="row about-container medium-uncollapse large-uncollapse">
   <div class="small-12 small-centered medium-10 columns large-uncollapse">
     <!--Mission Statement -->
-    <div class="small-12 mission-statement columns tac">
-      <h1 class="animated fadeInDown"><strong>Case Studies</strong></h1>
+    <div class="small-12 case-description-container columns tac">
+      <h1 class="animated fadeInDown">
+        <strong>Case Studies</strong>
+      </h1>
       <p>An inside look at how we approach and solve real business problems.</p>
       <p class="case-description">Our <a href="/portfolio">Portfolio</a> serves as a general overview of the work we've done, but we wanted a separate space to dive in and discuss our thought process when approaching client's problems.</p>
     </div>
@@ -12,24 +14,12 @@
 </div>
 <div class="case-study-container">
   <div class="row">
-    
+
     @foreach ($cases as $case)
      <p>This is user {{ $case -> id }}</p>
-    @endforeach
 
     <div class="small-12 medium-4 case-study-item columns">
-      <img src="http://placehold.it/350x350">
-    </div>
-    <div class="small-12 medium-4 case-study-item columns">
-      <img src="http://placehold.it/350x350">
-    </div>
-    <div class="small-12 medium-4 case-study-item columns">
-      <img src="http://placehold.it/350x350">
-    </div>
-  </div>
-  <div class="row">
-    <div class="small-12 medium-4 case-study-item columns">
-      <img src="http://placehold.it/350x350">
+      <a href="{{ $case->url }}"><img src="http://placehold.it/350x350"></a>
     </div>
     <div class="small-12 medium-4 case-study-item columns">
       <img src="http://placehold.it/350x350">
@@ -43,8 +33,7 @@
 <div class="
   small-12
   tac
-  columns">
-  <hr>
+  columns cases-cta boxed">
   <h1>Ready to begin?</h1>
   <h4>Let's build something great.</h4>
   <hr class="mini-hr">
