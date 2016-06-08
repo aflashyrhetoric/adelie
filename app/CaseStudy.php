555e423b2eee48d57eb438b389eacaac5738c927
Kevin Oh <aflashyrhetoric@gmail.com>
2016-06-08 13:47:59 -0400

    Commented out constructor for casestudy.

--- app/CaseStudy.php
+++ app/CaseStudy.php
@@ -21,16 +21,16 @@ class CaseStudy extends Model
   // And eloquent may not be able to guess the plural.
   protected $table = "casestudies";
 
-  public function __construct($fillable){
-    $this->client_name = $fillable['client_name'];
-    $this->about = $fillable['about'];
-    $this->tagline = $fillable['tagline'];
-    $this->thumbnail_url = $fillable['thumbnail_url'];
-    $this->hero_url = $fillable['hero_url'];
-    $this->phase_one = $fillable['phase_one'];
-    $this->phase_two = $fillable['phase_two'];
-    $this->phase_three = $fillable['phase_three'];
-  }
+  // public function __construct($fillable){
+  //   $this->client_name = $fillable['client_name'];
+  //   $this->about = $fillable['about'];
+  //   $this->tagline = $fillable['tagline'];
+  //   $this->thumbnail_url = $fillable['thumbnail_url'];
+  //   $this->hero_url = $fillable['hero_url'];
+  //   $this->phase_one = $fillable['phase_one'];
+  //   $this->phase_two = $fillable['phase_two'];
+  //   $this->phase_three = $fillable['phase_three'];
+  // }
 
   public function clientName(){
     return $this->client_name;
